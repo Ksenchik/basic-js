71222a318cee4002760db4b6cb35a02b6112ce42
Ksenchik <oksana.100@mail.ru>
2022-10-16 23:20:03 +0300

    feat: 4 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+//   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(typeof sampleActivity != 'string') return false;
+  if(isNaN(+sampleActivity)) return false;
+  if(+sampleActivity <= 0) return false;
+  if(+sampleActivity >= 15) return false;
+
+  res = Math.log(15/+sampleActivity) / (0.693 / 5730);
+  return Math.ceil(res)
 }
 
 module.exports = {
